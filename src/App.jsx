df3f926134697f721955458e9d8faa836722f835
Metker <64699075+metkerr@users.noreply.github.com>
2022-12-10 15:56:05 +0800

    update 70%

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,169 @@
+import { useEffect, useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import patternBg from "./images/pattern-bg.png";
+import "leaflet/dist/leaflet.css";
+import searchArrow from "./images/icon-arrow.svg";
+import markerIcon from "./images/icon-location.svg";
+import Leaflet from "leaflet";
+import axios from "axios";
+
 function App() {
-  return <div id="App">App</div>;
+  const [ipData, setIpData] = useState({});
+  const [searchIP, setSearchIP] = useState("");
+  const [useFlyTo, setUseFlyTo] = useState();
+
+  useEffect(() => {
+    //set timout to prevent trigger api everytime use type
+    const timer = setTimeout(() => {
+      axios(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=${
+          process.env.REACT_APP_KEY
+        }&${searchIP && `ipAddress=${searchIP}`}`
+      ).then((res) => {
+        setIpData(res.data);
+      });
+      console.log("api trigger");
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [searchIP]);
+
+  const HandleMapChange = () => {
+    const map = useMapEvents({
+      click: () => {
+        map.locate();
+      },
+      locationfound: (location) => {
+        console.log("location found:", location);
+      },
+    });
+    return null;
+  };
+
+  //create marker custom icon
+  const markerCustomIcon = new Leaflet.Icon({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon,
+    iconAnchor: null,
+    popupAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new Leaflet.Point(47, 56),
+    className: "bg-blend-color-burn",
+  });
+
+  console.log(ipData);
+
+  return (
+    <div id="App">
+      <header
+        className="h-76 bg-cover bg-center flex container"
+        style={{ backgroundImage: `url(${patternBg})` }}
+      >
+        <div
+          id="header-wrapper"
+          className="m-auto mt-7 flex flex-col w-full mx-6"
+        >
+          <h1 className="text-white text-2.5xl font-medium tracking-wide mx-auto mb-7">
+            IP Adress Tracker
+          </h1>
+          <div
+            id="search-input-wrapper"
+            className="bg-white rounded-2xl w-full flex"
+          >
+            <input
+              type="search"
+              className="grow rounded-2xl px-6 text-lg"
+              placeholder="Search for any IP address or domain"
+              value={searchIP}
+              onChange={(e) => setSearchIP(e.target.value)}
+            />
+            <button className="font-bold h-full bg-black text-white px-6 py-5.5 rounded-r-2xl text-xl">
+              <img src={searchArrow} alt="search arrow button" />
+            </button>
+          </div>
+          <div
+            id="details-wrapper"
+            className="rounded-2xl flex bg-white mt-6 shadow-md"
+            style={{ zIndex: 9999 }}
+          >
+            <div
+              id="details"
+              className="rounded-2xl bg-white p-7  flex flex-col mx-auto text-center gap-3.5 font-semibold text-very-dark-gray tracking-wide"
+            >
+              <div id="ip-address-wrapper">
+                <h3 className="text-2xs  text-dark-gray tracking-x-wide pb-1.5">
+                  IP ADDRESS
+                </h3>
+                <div className="text-xl" id="ip-address">
+                  {ipData?.ip || "Unknown"}
+                </div>
+              </div>
+              <div id="location-wrapper">
+                <h3 className="text-2xs text-dark-gray tracking-x-wide pb-1.5">
+                  LOCATION
+                </h3>
+                <div className="text-xl" id="location">
+                  {`${ipData?.location?.city || ""} ${
+                    ipData.location?.country ? "," : ""
+                  } ${ipData?.location?.country || "Unknown"} ${
+                    ipData?.location?.postalCode || ""
+                  }`}
+                </div>
+              </div>
+              <div id="timezone-wrapper">
+                <h3 className="text-2xs text-dark-gray tracking-x-wide pb-1.5">
+                  TIMEZONE
+                </h3>
+                <div className="text-xl" id="timezone">
+                  {ipData.location?.timezone
+                    ? `UTC${ipData.location.timezone}`
+                    : "Unknown"}
+                </div>
+              </div>
+              <div id="isp-wrapper">
+                <h3 className="text-2xs text-dark-gray tracking-x-wide pb-1.5">
+                  ISP
+                </h3>
+                <div className="text-xl" id="isp">
+                  {ipData.isp ? ipData.isp : "Unknown"}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div id="map-wrapper">
+          <div id="map" className="overflow-auto h-full">
+            <MapContainer
+              center={[-6.1752, 106.8272]}
+              zoom={13}
+              scrollWheelZoom={true}
+              zoomControl={false}
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[-6.1752, 106.8272]} icon={markerCustomIcon}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "very-dark-gray": "hsl(0, 0%, 17%)",
+        "dark-gray": "hsl(0, 0%, 59%)",
+      },
+      height: {
+        76: "19rem",
+      },
+      letterSpacing: {
+        "x-wide": "0.17em",
+      },
+      fontSize: {
+        "2xs": "0.6rem",
+        "2.5xl": "1.625rem",
+      },
+      padding: {
+        5.5: "1.32rem",
+      },
+    },
   },
   plugins: [],
 };
